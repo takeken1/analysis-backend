5d82455e8470eb120d7afbe30129c8d1c95da0b1
takeken1 <takeken1@gmail.com>
2024-03-09 22:56:23 +0900

    first commit.

---
+++ src/test/java/com/example/analysis/AnalysisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalysisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
