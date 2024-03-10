0c3b31531b54d010b68b1812e9ef2610da117027
takeken1 <takeken1@gmail.com>
2024-03-10 23:00:55 +0900

    Update GETのメソッドは使わないので削除

--- src/main/java/com/example/analysis/controller/AnalysisController.java
+++ src/main/java/com/example/analysis/controller/AnalysisController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,14 +25,8 @@ public class AnalysisController {
         return ResponseEntity.ok(analysisService.models());
     }
 
-    @GetMapping("/chat")
-    public ResponseEntity<?> chat(@ModelAttribute AnalysisForm form) {
-        CompletionsResponse response = analysisService.chat(form);
-        return ResponseEntity.ok(response);
-    }
-
     @PostMapping("/chat")
-    public ResponseEntity<?> chatPost(@RequestBody AnalysisForm form) {
+    public ResponseEntity<?> chat(@RequestBody AnalysisForm form) {
         CompletionsResponse response = analysisService.chat(form);
         return ResponseEntity.ok(response);
     }
